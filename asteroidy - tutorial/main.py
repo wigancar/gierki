c4498462dfde3e9457dd2acbc7d820a5281f96ef
wigancar <wiktorwksgancarczyk@wp.pl>
2022-04-19 13:55:04 +0200

    Początek pierwszej gry z tutoriala.

---
+++ asteroidy - tutorial/main.py
@@ -0,0 +1,116 @@
+import math
+
+import pygame
+
+
+screen_w = 800
+screen_h = 800
+
+background = pygame.image.load('asteroidsPics/starbg.png')
+alienImg = pygame.image.load('asteroidsPics/alienShip.png')
+playerRocket = pygame.image.load('asteroidsPics/spaceRocket.png')
+star = pygame.image.load('asteroidsPics/star.png')
+asteroid50 = pygame.image.load('asteroidsPics/asteroid50.png')
+asteroid100 = pygame.image.load('asteroidsPics/asteroid100.png')
+asteroid150 = pygame.image.load('asteroidsPics/asteroid150.png')
+
+pygame.display.set_caption('Asteroids')
+win = pygame.display.set_mode((screen_w, screen_h))
+
+clock = pygame.time.Clock()
+
+gameover = False
+
+
+class Player(object):
+    def __init__(self):
+        self.img = playerRocket
+        self.w = self.img.get_width()
+        self.h = self.img.get_height()
+        self.x = screen_w//2
+        self.y = screen_h//2
+        self.angle = 0
+        self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
+        self.rotatedRect = self.rotatedSurf.get_rect()
+        self.rotatedRect.center = (self.x, self.y)
+        self.cosine = math.cos(math.radians(self.angle + 90))
+        self.sine = math.sin(math.radians(self.angle + 90))
+        self.head = (self.x + self.cosine * self.w//2, self.y - self.sine * self.h//2)
+
+    def draw(self, window):
+        # window.blit(self.img, [self.x, self.y, self.w, self.h])
+        window.blit(self.rotatedSurf, self.rotatedRect)
+
+    def turnLeft(self):
+        self.angle += 5
+        self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
+        self.rotatedRect = self.rotatedSurf.get_rect()
+        self.rotatedRect.center = (self.x, self.y)
+        self.cosine = math.cos(math.radians(self.angle + 90))
+        self.sine = math.sin(math.radians(self.angle + 90))
+        self.head = (self.x + self.cosine * self.w//2, self.y - self.sine * self.h//2)
+
+    def turnRight(self):
+        self.angle -= 5
+        self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
+        self.rotatedRect = self.rotatedSurf.get_rect()
+        self.rotatedRect.center = (self.x, self.y)
+        self.cosine = math.cos(math.radians(self.angle + 90))
+        self.sine = math.sin(math.radians(self.angle + 90))
+        self.head = (self.x + self.cosine * self.w//2, self.y - self.sine * self.h//2)
+
+    def moveForward(self):
+        self.x += self.cosine * 6
+        self.y -= self.sine * 6
+        self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
+        self.rotatedRect = self.rotatedSurf.get_rect()
+        self.rotatedRect.center = (self.x, self.y)
+        self.cosine = math.cos(math.radians(self.angle + 90))
+        self.sine = math.sin(math.radians(self.angle + 90))
+        self.head = (self.x + self.cosine * self.w//2, self.y - self.sine * self.h//2)
+
+
+class Bullet(object):
+    def __init__(self):
+        self.point = player.head
+        self.x, self.y = self.point
+        self.w = 4
+        self.h = 4
+        self.c = player.cosine
+        self.s = player.sine
+        self.xv = self.c * 10
+        self.yv = self.s * 10
+
+    def move(self):
+        self.x += self.xv
+        self.y += self.yv
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255, 255, 255), [self.x, self.y, self.w, self.h])
+
+
+def redrawGameWindow():
+    win.blit(background, (0, 0))
+    player.draw(win)
+    pygame.display.update()
+
+
+player = Player()
+run = True
+while run:
+    clock.tick(60)
+    if not gameover:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player.turnLeft()
+        if keys[pygame.K_RIGHT]:
+            player.turnRight()
+        if keys[pygame.K_UP]:
+            player.moveForward()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    redrawGameWindow()
+pygame.quit()
